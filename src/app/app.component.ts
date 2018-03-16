ca95d80ff87d7c216aaa0b82eeb06170497d228c
Duong Do <duongd@aareon.nl>
2018-03-16 10:37:25 +0100

    Simple code for Autocomplete only

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,31 +13,22 @@ import { CompleterService, CompleterData, LocalData, RemoteData } from 'ng-mdb-p
 })
 export class AppComponent implements OnInit {
   private countries: any;
-  private today: Date;
-  private maxBirthDateStr: string;
-  private netherlandText: string;
   private selectedCountryName: string;
 
-  private birthDate: Date;
   private countryDS: CompleterData;
 
   constructor(
     private completerService: CompleterService,
     private appService: AppService) {
-    
   }
   ngOnInit() {
-    this.today = new Date(1989, 12, 10);
-    this.birthDate = new Date(1989, 0, 12);
-    let datePipe = new DatePipe("en-US");
-    this.maxBirthDateStr = datePipe.transform(new Date(), 'yyyy-MM-dd');
-    this.appService.getCountries().subscribe(data => this.fillCountryDS(data), error => console.log("Error :: " + error));
+    this.appService.getCountries().subscribe(data => this.fillCountryDS(data), error => console.log('Error :: ' + error));
   }
 
   fillCountryDS(data) {
     this.countries = data;
     this.countryDS = this.completerService.local(data, 'name,code', 'name');
-    this.netherlandText = this.selectedCountryName = this.countries.filter(c => c.code === "NL")[0].name;
+    this.selectedCountryName = this.countries.filter(c => c.code === 'NL')[0].name;
   }
 
   onTextChangeCountry() {
